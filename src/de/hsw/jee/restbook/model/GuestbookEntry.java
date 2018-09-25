faa962551d38530daa088f7eb667c048a33a9988
Malte Wildt <malte.wildt@flavia-it.de>
2018-09-25 16:34:46 +0200

    reformat

--- src/de/hsw/jee/restbook/model/GuestbookEntry.java
+++ src/de/hsw/jee/restbook/model/GuestbookEntry.java
@@ -3,47 +3,58 @@ package de.hsw.jee.restbook.model;
 import java.util.Date;
 
 public class GuestbookEntry {
-	
+
 	private Long id;
 	private String username;
 	private String title;
 	private String message;
 	private Date created;
 	private Date modified;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
 	public Date getCreated() {
 		return created;
 	}
+
 	public void setCreated(Date created) {
 		this.created = created;
 	}
+
 	public Date getModified() {
 		return modified;
 	}
+
 	public void setModified(Date modified) {
 		this.modified = modified;
 	}
@@ -52,11 +63,9 @@ public class GuestbookEntry {
 		this.created = new Date();
 		this.preUpdate();
 	}
-	
+
 	public void preUpdate() {
 		this.modified = new Date();
 	}
-	
-	
-	
+
 }
--- src/de/hsw/jee/restbook/services/GuestbookService.java
+++ src/de/hsw/jee/restbook/services/GuestbookService.java
@@ -2,7 +2,6 @@ package de.hsw.jee.restbook.services;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
